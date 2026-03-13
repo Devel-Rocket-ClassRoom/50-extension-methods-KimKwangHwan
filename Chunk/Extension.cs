171a9f59822e6f2173339a27f604431c67b30a57
KimKwangHwan <louie1004@naver.com>
2026-03-13 18:02:14 +0900

    chunk

---
+++ Chunk/Extension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class EnumerableExtension
+{
+    public static IEnumerable<IEnumerable<T>> Chunk<T>(this IEnumerable<T> items, int size)
+    {
+        if (items.Count<T>() < 1)
+        {
+            throw new ArgumentException();
+        }
+        List<T> list = new List<T>(size);
+        int count = 0;
+        foreach (var item in items)
+        {
+            list.Add(item);
+            count++;
+            if (count == size)
+            {
+                yield return list.ToList<T>();
+                list.Clear();
+                count = 0;
+            }
+        }
+        if (list.Count > 0)
+        {
+            yield return list.ToList<T>();
+        }
+    }
+}
--- Chunk/Program.cs
+++ Chunk/Program.cs
@@ -2,5 +2,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-// README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var test1 = numbers.Chunk<int>(3);
+foreach (var i in test1)
+{
+    Console.WriteLine($"[{string.Join(", ", i)}]");
+}
+Console.WriteLine();
+
+string[] names = new string[] { "김철수", "이영희", "박민수", "최지연", "정우진" };
+var test2 = names.Chunk<string>(2);
+foreach (var s in test2)
+{
+    Console.WriteLine($"[{string.Join(", ", s)}]");
+}
+Console.WriteLine();
+
+int[] pages = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25 };
+var test3 = pages.Chunk<int>(5);
+foreach (var i in test3)
+{
+    Console.WriteLine($"[{string.Join(", ", i)}]");
+}
+Console.WriteLine();
